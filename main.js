0b95245f857af6a339e1ec0a07844277c8f62891
emma31-dev <emmanuelfidel07@gmail.com>
2025-12-07 10:54:07 +0100

    fixed prisma issue

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { Elysia, t } = require("elysia");
-const { PrismaClient } = require("@prisma/client");
-const { nanoid } = require("nanoid")
+import "dotenv/config";
+import { Elysia, t } from "elysia";
+import { PrismaClient } from "./generated/client.ts";
+import { nanoid } from "nanoid";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 const app = new Elysia();
 
 // Endpoint to create a shortened URL
@@ -12,17 +13,17 @@ app.post(
     const shortCode = nanoid(8);
 
     try {
-      const newUrl = await prisma.url.create({
+      const newUrl = await prisma.link.create({
         data: {
           shortCode: shortCode,
-          long_url: body.url
+          longUrl: body.url
         }
       });
       set.status = 201;
       return {
         message: "Short URL created successfully",
         shortUrl: `${process.env.URL}${newUrl.shortCode}`,
-        long_url: newUrl.long_url
+        longUrl: newUrl.longUrl
     };
     } catch (error) {
       set.status = 500;
@@ -47,7 +48,7 @@ app.post(
 app.get(
   "/:shortCode",
   async ({ params, set }) => {
-    const link = await prisma.url.findUnique({
+    const link = await prisma.link.findUnique({
       where: {
         shortCode: params.shortCode,
       },
@@ -58,7 +59,7 @@ app.get(
       return { message: "Short URL not found" };
     }
     set.status = 302;
-    set.headers['Location'] = link.long_url;
+    set.headers['Location'] = link.longUrl;
     return;
   },
 
@@ -73,6 +74,9 @@ app.get(
   }
 );
 
-app.listen(3000, () => {
+import { createServer } from "http";
+
+const server = createServer(app.fetch);
+server.listen(3000, () => {
   console.log(`URL Shortener service is running on http://localhost:3000`);
 });
